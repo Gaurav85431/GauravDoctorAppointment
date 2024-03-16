090ea7ad67d5f3e2415305a96a6dbdbce9608e24
Gaurav Pushpam <145956263+Gaurav85431@users.noreply.github.com>
2024-03-16 21:51:17 +0530

    Your commit message

--- config/db.js
+++ config/db.js
@@ -5,7 +5,11 @@ const connectDB = async () => {
   try {
 
     // await mongoose.connect(process.env.MONGO_URL)
-    await mongoose.connect('mongodb+srv://pushpamgaurav3:JVbhKHVXDBFWebD0@cluster0.mlmlkis.mongodb.net/?retryWrites=true&w=majority')
+
+    await mongoose.connect('mongodb+srv://pushpamgaurav3:BcQyJf5zWg4ruaMJ@doctor.5hfywgd.mongodb.net/?retryWrites=true&w=majority&appName=Doctor');
+
+    //local server ka db
+    // await mongoose.connect('mongodb://localhost:27017/DoctorAppointment')
 
     //local server ka db
     // await mongoose.connect('mongodb://localhost:27017/DoctorAppointment')
